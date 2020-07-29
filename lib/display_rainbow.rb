4dd1637468a8ec35c08c3378c1c2314a63f78f62
Glenn Jensen <g.e.jensen@gmail.com>
2020-07-29 14:03:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,14 +3,16 @@
 def display_rainbow(color_array=["hot pink","horse"])
 
 counter = 0
+entries = color_array.count
 
-puts "this is where your colors will go:  #{color_array[0]}, #{color_array[1]}"
+while counter <= entries do
 
+  puts "the counter is at #{counter}"
+  puts "the array has #{color_array.count} entries"
+  counter = counter + 1
 
-counter = counter + 1
-
-puts "the counter is at #{counter}"
-puts "the array has #{color_array.count} entries"
+end
+# puts "this is where your colors will go:  #{color_array[0]}, #{color_array[1]}"
 
 end
 
