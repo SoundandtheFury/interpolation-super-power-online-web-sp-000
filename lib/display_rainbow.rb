cdbd07e454b222776b446356a7d9b7cb0a0b8088
Glenn Jensen <g.e.jensen@gmail.com>
2020-07-29 13:55:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 
 def display_rainbow(color_array=["hot pink","horse"])
 
-print "this is where your colors will go"
+print "this is where your colors will go:  #{color_array[0]}, #{color_array[1]}"
 
 end
 
